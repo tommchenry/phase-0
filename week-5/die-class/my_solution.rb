dd3ea012fd13f89bfc46d1ed3c89f928a138a6d1
Tom McHenry <tommchenry@gmail.com>
2015-11-19 19:22:56 -0600

    Add basic methods.

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -1,13 +1,13 @@
 # Die Class 1: Numeric
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge by myself.
 
 # I spent [] hours on this challenge.
 
 # 0. Pseudocode
 
-# Input:
-# Output:
+# Input: a new instance of the class Die with a number of sides
+# Output: the output of sides is the number of sides the die object has, the output of roll is a random number between 1 and the number of sides
 # Steps:
 
 
@@ -16,18 +16,23 @@
 class Die
   def initialize(sides)
     # code goes here
+    @sides = sides
   end
 
   def sides
     # code goes here
+    p @sides
   end
 
   def roll
     # code goes here
+    p rand(1..@sides)
   end
 end
 
-
+# die = Die.new(6)
+# die.sides
+# die.roll
 
 # 3. Refactored Solution
 
