fd9123cc5ead86e5badd8558e7967d96adca6f21
Tom McHenry <tommchenry@gmail.com>
2015-12-05 23:14:55 -0600

    Add reflection.

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -61,12 +61,13 @@ function Athlete(name, age, sport, quote){
 var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
 console.log(michaelPhelps.constructor === Athlete)
 console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
-// Build the constructor function that will make this driver code work.
-
 
 
 
 // Reflection
 // What JavaScript knowledge did you solidify in this challenge?
+//  The ability of a function to create additional properties of an object. It hadn't occurred to me that we could iterate through an array of objects, adding properties like that. There's also a solution we wound up not using where we tried to .pop the odd values off of the array of even numbers and return the remaining array, but we learned you can't pop a value by the index -- .pop only removes the last value in the array, so each successive call shortens it from the ned.
 // What are constructor functions?
+//  Constructor functions have slightly different syntax than object literals. They function very much like classes in Ruby in that constructor functions let you build multiple instances (objects), each with their own separate property values and methods. Additionally, once an instance is constructed with the constructor, an individual instance's properties can be changed without affecting all other instances of the same object.
 // How are constructors different from Ruby classes (in your research)?
+//  One of the most obvious differences between Javascript constructors and Ruby classes is the use of the keyword this. Using .this is sort of similar to the @ sign on instance variables, but .this is specifying this specific object's given "name" property value (for example). They also aren't technically classes, in that they don't have class-specific methods the way Ruby does, the constructor objects are still just objects.
