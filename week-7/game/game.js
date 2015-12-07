e9aaa378ce6f1c64f3a6f53be28b6cae6675a4e2
Tom McHenry <tommchenry@gmail.com>
2015-12-06 21:17:44 -0600

    Add refactoring.

--- week-7/game/game.js
+++ week-7/game/game.js
@@ -21,8 +21,150 @@
 //  If the player moves onto the spaghetti square, the player eats the spaghetti.
 //  If the cop moves adjacent to the player, the player is arrested.
 
-// Initial Code
+// // Initial Code
 
+
+// // This function generates an empty level tabular array and then populates it with the player, a plate of spaghetti, and the cop.
+// function createLevel(){
+//   var levelWidth = 8;
+//   var levelHeight = 8;
+//   var level = [];
+//   for (var row = 0; row < levelHeight; row ++){
+//     var temp_row = [];
+//     for (var square = 0; square < levelWidth; square++) {
+//       temp_row.push(0);
+//     }
+//     level.push(temp_row);
+//   }
+//   level[player.yPos][player.xPos] = "@";
+//   level[cop1.yPos][cop1.xPos] = "C";
+//   level[spaghetti1.yPos][spaghetti1.xPos] = "S";
+//   return level;
+// }
+
+// var player = {
+//   xPos: 7,
+//   yPos: 7,
+// };
+
+// function Spaghetti(xPos, yPos, pasta){
+//   this.xPos = xPos;
+//   this.yPos = yPos;
+//   this.pasta = pasta;
+// }
+
+// function Cop(xPos, yPos, speed){
+//   this.xPos = xPos;
+//   this.yPos = yPos;
+//   this.speed = speed;
+// }
+
+// // Displays Level to Console
+// // Moves through the level array and concatenates a string to display to console.
+// function displayLevel(level){
+//   toDisplay = "";
+//   for (row= 0; row < level.length; row++){
+//     for (square = 0; square < level[row].length; square++){
+//       switch(level[row][square]) {
+//         case 0:
+//           toDisplay += ".";
+//           break;
+//         case "@":
+//           toDisplay += "@";
+//           break;
+//         case "C":
+//           toDisplay += "C";
+//           break;
+//         case "S":
+//           toDisplay += "S";
+//           break;
+//       }
+//     }
+//     toDisplay += "\n";
+//   }
+//   console.log(toDisplay);
+// }
+
+// // Cop Movement
+// // If the cop is further away on the x-axis the cop moves closer on that axis, otherwise moves closer on the y-axis.
+// function copMove(cop){
+//   if (Math.abs(cop.xPos - player.xPos) <= Math.abs(cop.yPos - player.yPos)){
+//       level[cop.yPos][cop.xPos] = 0;
+//       if (cop.xPos > player.xPos){
+//         cop.xPos -= 1;
+//       }
+//       else {
+//         cop.xPos += 1;
+//       }
+//       level[cop.yPos][cop.xPos] = "C";
+//       catchDog(cop);
+//   }
+//     else{
+//         level[cop.yPos][cop.xPos] = 0;
+//         if (cop.yPos > player.yPos){
+//           cop.yPos -= 1;
+//         }
+//         else {
+//           cop.yPos += 1;
+//         }
+//         level[cop.yPos][cop.xPos] = "C";
+//         catchDog(cop);
+//     }
+// }
+
+// // If the cop is ever adjacent to the player, the cop arrests the player and the game ends.
+// function catchDog(cop){
+//     console.log(Math.abs(player.xPos - cop.xPos));
+//     console.log(Math.abs(player.xPos - cop.xPos));
+//   if (Math.abs(player.xPos - cop.xPos) == 1 && Math.abs(player.yPos - cop.yPos) == 1){
+//     console.log("Ya Busted!");
+//   }
+// }
+
+// function eatSpaghetti(spaghetti){
+//   if (player.yPos == spaghetti.yPos && player.xPos == spaghetti.xPos){
+//     console.log("Yum, yum! I love that spaghetti!");
+//   }
+// }
+
+// // Player Movement Functions
+// function moveUp() {
+//   if (player.yPos - 1 >= 0){
+//     level[player.yPos][player.xPos] = 0;
+//     player.yPos -= 1;
+//     level[player.yPos][player.xPos] = "@";
+//   }
+//   eatSpaghetti(spaghetti1);
+// }
+
+// function moveDown() {
+//   if (player.yPos + 1 < level.length){
+//     level[player.yPos][player.xPos] = 0;
+//     player.yPos += 1;
+//     level[player.yPos][player.xPos] = "@";
+//   }
+//   eatSpaghetti(spaghetti1);
+// }
+
+// function moveRight() {
+//   if (player.xPos + 1 < level[0].length){
+//     level[player.yPos][player.xPos] = 0;
+//     player.xPos += 1;
+//     level[player.yPos][player.xPos] = "@";
+//   }
+//   eatSpaghetti(spaghetti1);
+// }
+
+// function moveLeft() {
+//   if (player.xPos - 1 >= 0){
+//     level[player.yPos][player.xPos] = 0;
+//     player.xPos -= 1;
+//     level[player.yPos][player.xPos] = "@";
+//   }
+//   eatSpaghetti(spaghetti1);
+// }
+
+// Refactored Code
 
 // This function generates an empty level tabular array and then populates it with the player, a plate of spaghetti, and the cop.
 function createLevel(){
@@ -45,6 +187,7 @@ function createLevel(){
 var player = {
   xPos: 7,
   yPos: 7,
+  pastaPoints: 0,
 };
 
 function Spaghetti(xPos, yPos, pasta){
@@ -114,8 +257,6 @@ function copMove(cop){
 
 // If the cop is ever adjacent to the player, the cop arrests the player and the game ends.
 function catchDog(cop){
-    console.log(Math.abs(player.xPos - cop.xPos));
-    console.log(Math.abs(player.xPos - cop.xPos));
   if (Math.abs(player.xPos - cop.xPos) == 1 && Math.abs(player.yPos - cop.yPos) == 1){
     console.log("Ya Busted!");
   }
@@ -124,44 +265,41 @@ function catchDog(cop){
 function eatSpaghetti(spaghetti){
   if (player.yPos == spaghetti.yPos && player.xPos == spaghetti.xPos){
     console.log("Yum, yum! I love that spaghetti!");
+    player.pastaPoints += spaghetti.pasta;
   }
 }
 
 // Player Movement Functions
 function moveUp() {
   if (player.yPos - 1 >= 0){
-    level[player.yPos][player.xPos] = 0;
-    player.yPos -= 1;
-    level[player.yPos][player.xPos] = "@";
+    playerMover(0,-1);
   }
-  eatSpaghetti(spaghetti1);
 }
 
 function moveDown() {
   if (player.yPos + 1 < level.length){
-    level[player.yPos][player.xPos] = 0;
-    player.yPos += 1;
-    level[player.yPos][player.xPos] = "@";
+    playerMover(0,1);
   }
-  eatSpaghetti(spaghetti1);
 }
 
 function moveRight() {
   if (player.xPos + 1 < level[0].length){
-    level[player.yPos][player.xPos] = 0;
-    player.xPos += 1;
-    level[player.yPos][player.xPos] = "@";
+    playerMover(1,0);
   }
-  eatSpaghetti(spaghetti1);
 }
 
 function moveLeft() {
   if (player.xPos - 1 >= 0){
-    level[player.yPos][player.xPos] = 0;
-    player.xPos -= 1;
-    level[player.yPos][player.xPos] = "@";
+    playerMover(-1,0);
   }
-  eatSpaghetti(spaghetti1);
+}
+
+function playerMover(deltaxPos, deltayPos){
+    level[player.yPos][player.xPos] = 0;
+    player.xPos += deltaxPos;
+    player.yPos += deltayPos;
+    level[player.yPos][player.xPos] = "@";
+    eatSpaghetti(spaghetti1);
 }
 
 // console.log(displayLevel(level));
@@ -193,11 +331,6 @@ displayLevel(level);
 // displayLevel(level);
 // copMove(cop1);
 // displayLevel(level);
-
-
-// Refactored Code
-
-
 
 
 
